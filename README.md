78d1e3732bb14ec9f3cb77bc2fbc45e0287e709d
Rebecca McFadden <rem20806@tamu.edu>
2019-07-25 08:45:12 -0700

    publish OnEdge

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# website
+# Offline game for Edge Chromium Browser
+### 2019 Microsoft Hackathon Project
+Don't fall off the "edge" ;)
---
+++ platform.js
@@ -0,0 +1,44 @@
+PlatformImg = new Image();
+PlatformImg.src = 'images/platform.png'
+
+function Platform(game){
+    // Singleton
+    if(Platform.instance_){
+        return Platform.instance_
+    }
+    this.instance_ = this
+    this.Game = game
+
+    this.canvasCtx = null
+    this.canvas = null
+
+	this.platformHeight = 0
+    this.platformWidth = 0
+    this.platformX = 0
+    this.platformY = 0
+    
+    this.init()
+}
+
+Platform.prototype = {
+    init: function(){
+        this.canvasCtx = this.Game.canvasCtx
+        this.canvas = this.canvasCtx.canvas
+
+        this.platformHeight = 16       
+        this.platformWidth = this.canvas.width * 3 / 5
+        this.platformX =  (this.canvas.width / 5)  /*left bound of the platform  */
+        this.platformY = this.canvas.height - this.platformHeight
+        },
+    draw: function(){
+        // this.canvasCtx.beginPath();
+        // this.canvasCtx.rect(this.platformX, this.canvas.height - this.platformHeight, this.platformWidth, this.platformHeight);
+        // this.canvasCtxfillStyle = "#0095DD";
+        // this.canvasCtx.fill();
+        // this.canvasCtx.closePath();
+        this.canvasCtx.drawImage(PlatformImg, this.platformX, this.platformY, this.platformWidth, this.platformHeight);
+    },
+    update: function(){
+        this.draw()
+    }
+}
